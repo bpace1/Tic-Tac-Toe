a2148c3ed8aea5d0f69199b6dbe3d458a73c8e70
Bruno Pace <brunopace2001@gmail.com>
2025-01-24 09:43:35 -0300

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from random import randrange
 
 board_type = list[list[int | str, int | str, int | str], list[int | str, int | str, int | str], list[int | str, int | str, int | str]] 
 
@@ -46,9 +47,12 @@ def make_list_of_free_fields(board: board_type) -> list[Tuple[int, int]]:
     """
     free_squares: list[Tuple[int, int]] = []
     for i, row in enumerate(board):
+        #print(f'i: {i}, row: {row}')
+        count: int = 0
         for col in row:
             if type(col) == int:
-                free_squares.append((i+1, col))
+                free_squares.append((i, count))
+            count += 1
     return free_squares
 
 def square_to_tuple(square: int) -> Tuple[int, Tuple[int, int]]:
@@ -64,15 +68,15 @@ def square_to_tuple(square: int) -> Tuple[int, Tuple[int, int]]:
     """
 
     corresponding_tuple: dict[int, Tuple[int, int]] = {
-        1: (0, 1),
-        2: (0, 2),
-        3: (0, 3), 
-        4: (1, 1), 
-        5: (1, 2), 
-        6: (1, 3), 
-        7: (2, 1), 
-        8: (2, 2), 
-        9: (2, 3)
+        1: (0, 0),
+        2: (0, 1),
+        3: (0, 2), 
+        4: (1, 0), 
+        5: (1, 1), 
+        6: (1, 2), 
+        7: (2, 0), 
+        8: (2, 1), 
+        9: (2, 2)
         }  
     return square, corresponding_tuple[square]
 
@@ -89,7 +93,7 @@ def valid_movement(board: board_type, square: Tuple[int, int]) -> bool:
         - True if the square is a valid movement, False otherwise.
     """
     free_squares: list[Tuple[int, int]] = make_list_of_free_fields(board)
-
+    #print(free_squares)
     if square in free_squares:
         return True
     else:
@@ -107,26 +111,64 @@ def make_movement(board: board_type, square: int, turn: str) -> board_type:
         - board: A list of lists that represents the board after the movement.
     
     """
-    square_tuple: Tuple[int, int] = square_to_tuple(square)
-
-    board[square_tuple[0]][square_tuple[1]] = turn
+    board[square[0]][square[1]] = turn
 
     return board
 
-def victory_for(board, sign):
-    # The function analyzes the board's status in order to check if 
-    # the player using 'O's or 'X's has won the game
-    pass
+def all_same(lista: list) -> bool:
+    """
+    Checks if all elements in a list are equals
+
+    args: 
+        - lista: a list of elements.
+    
+    Returns:
+        - bool: True if the elements of the list are equals or False if they are not.
+    """
+    return len(set(lista) == 1)
+
+def victory_for(board):
+    for row in board:
+        if all_same(row):
+            return True
+            
+    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+        return True
+    
+    vertical_0: bool = board[0][0] == board[1][0] == board[2][0]
+    vertical_1: bool = board[0][1] == board[1][1] == board[2][1]
+    vertical_2: bool = board[0][2] == board[1][2] == board[2][2]
+
+    if vertical_0 or vertical_1 or vertical_2:
+        return True        
+    
+    return False
+
 
 def __main__():
     board: board_type = create_board()
-    free_squares: list[Tuple[int, int]] = make_list_of_free_fields(board)
-    move = 7
+
+    while True:
+        count: int = 1
+
+        display_board(board)
+
+        move: str = int(input('Ingrese un movimiento en una casilla disponible:'))
+
+        _, square = square_to_tuple(move)
+
+        if count % 2 != 0:
+            pass
+        if valid_movement(board,square):
+
+            board = make_movement(board,square,'O')
+            display_board(board)
+            if victory_for(board):
+                return
+        else:
+            print('movimiento invalido')
+            continue
 
     
-  
-    
-    
-
 if __name__ == "__main__":
     __main__()
